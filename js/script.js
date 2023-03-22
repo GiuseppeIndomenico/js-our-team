9ff483be468a1505c1da809471112caac2188074
Giuseppe Indomenico <pippo.son@hotmail.com>
2023-03-22 14:57:24 +0100

    first try for in

--- js/script.js
+++ js/script.js
@@ -5,3 +5,42 @@
 // Angela   Lopez	    Social Media Manager	  angela-lopez-social-media-manager.jpg
 // Scott    Estrada	Developer	              scott-estrada-developer.jpg
 // Barbara  Ramos	    Graphic Designer	      barbara-ramos-graphic-designer.jpg
+
+
+
+    const team = [
+        worker = {
+            name:'Wayne',
+            surname:' Barnett',
+            role:'Founder & CEO',
+            image:'wayne-barnett-founder-ceo.jpg' 
+
+        },
+
+        worker1 = {
+            name:'Angela ',
+            surname:' Caroll',
+            role:'Chief Editor',
+            image:'angela-caroll-chief-editor.jpg' 
+
+        },
+        worker2 = {
+            name:'Walter ',
+            surname:'  Gordon',
+            role:'Office Manager',
+            image:' walter-gordon-office-manager.jpg' 
+
+        },
+
+    ]
+
+    for (const key in team) {
+               const element = team[key];
+        
+               console.log(element);
+        }
+    
+
+
+
+    
